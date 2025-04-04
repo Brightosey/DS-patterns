bad6512c86ca1b316fd41d77ac4d8662c1a790ce
Brightosey <Brightosey@users.noreply.github.com>
2025-04-04 01:38:34 -0400

    two pionter pattern

--- twoPointer.js
+++ twoPointer.js
@@ -44,10 +44,10 @@ function twoSum(nums, target) {
 
     // Check if the complement is already in the map
     if (map.has(complement)) {
-      return [map.get(complement), i];  // Return indices of the two numbers
+      return [map.get(complement), i]; // Return indices of the two numbers
     }
 
-    map.set(nums[i], i);  // Store the current number and its index
+    map.set(nums[i], i); // Store the current number and its index
   }
 
   return []; // In case no solution is found
@@ -55,32 +55,58 @@ function twoSum(nums, target) {
 
 //Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
-function removeDuplicate (nums) {
-    if (nums.length === 0) return 0; //Edge case
+function removeDuplicate(nums) {
+  if (nums.length === 0) return 0; //Edge case
 
-    let i = 0; //initializing the pointer i
+  let i = 0; //initializing the pointer i
 
-    for (let j = 1; j<nums.length; j++) {
-        if (nums[j] !== nums[i]) {  // found a unique number
-            i++;
-            nums[i] = nums[j];  // replace index i with index j which overides duplicates
-        }
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] !== nums[i]) {
+      // found a unique number
+      i++;
+      nums[i] = nums[j]; // replace index i with index j which overides duplicates
     }
+  }
 
-    return i + 1; // returns number of unique elements in the array
+  return i + 1; // returns number of unique elements in the array
 }
 
 //Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
-function removeElement (nums,val) {
-    let i = 0;
+function removeElement(nums, val) {
+  let i = 0;
 
-    for (let j=0; j<nums.length; j++) {
-        if (nums[j] !== val) {
-            nums[i] = nums[j];
-            i++;
-        }
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+
+  return i;
+}
+
+//Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+function sortedSquare(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  let array = new Array(nums.length);
+  let index = nums.length - 1;
+
+  while (left <= right) {
+    let leftSquare = nums[left] * nums[left];
+    let rightSquare = nums[right] * nums[right];
+
+    if (leftSquare > rightSquare) {
+      array[index] = leftSquare;
+      left++;
+    } else {
+      array[index] = rightSquare;
+      right--;
     }
 
-    return i;
+    index--;
+  }
+  return array;
 }
